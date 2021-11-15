d4864754cc7ba599d0d2e283573f6c44813c358d
Salman Khondker <salmankhondker1998@gmail.com>
2021-11-16 00:22:18 +0600

    render html, templates, bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+# url_for locates locations of files, instead of putting them in manually.
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Corey Schafer",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2018"
+    },
+    {
+        "author": "Salman Khondker",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "April 21, 2018"
+    }
+]
+
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
